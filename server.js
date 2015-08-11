f9c54cb5c3d47b0610c4768b23590d57a56c458e
CodeMaster7 <italianstalian_2@yahoo.com>
2015-08-11 13:06:57 -0600

    compleated step 1

---
+++ server.js
@@ -0,0 +1,11 @@
+//npm init
+//npm install express body-parser cors mongojs --save
+var express = require('express');
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+
+//How to connect to database:
+
