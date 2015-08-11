f0076656497b8107a7d5b2a9d4fc9a1a9e64a5b4
CodeMaster7 <italianstalian_2@yahoo.com>
2015-08-11 13:08:14 -0600

    compleated step 2

--- server.js
+++ server.js
@@ -9,3 +9,30 @@ var app = express();
 
 //How to connect to database:
 
+//Run mongod 
+//var db = mongoJS('birds', ['sightings']);
+var db = mongojs('birds', ['sightings']);
+//var port = [mongod port from cmd];
+var port = 27017;
+
+app.post('/api/sighting', function(req, res){
+    console.log('post hit');
+    res.end();
+})
+app.get('/api/sighting', function(req, res){
+    console.log('get hit');
+    res.end();
+})
+app.delete('/api/sighting', function(req, res){
+    console.log('delete hit');
+    res.end();
+})
+app.put('/api/sighting', function(req, res){
+    console.log('put hit');
+    res.end();
+})
+
+
+app.listen(3000, function(){
+    
+});
